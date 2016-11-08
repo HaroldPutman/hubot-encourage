de6f34b105340e49f4ee3c000a79f43082745808
Harold Putman <harold.putman@lexmark.com>
2016-11-08 11:53:08 -0500

    Get the tests working on older versions of node

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var Helper = require('hubot-test-helper');
-var expect = require('chai').expect;
+const Helper = require('hubot-test-helper');
+const expect = require('chai').expect;
 
-var helper = new Helper('../src/index.js'); // path to file you want to test
+const helper = new Helper('../src/index.js'); // path to file you want to test
 
-var NewMockResponse;
-var hasProp = {}.hasOwnProperty;
-var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+let NewMockResponse;
+const hasProp = {}.hasOwnProperty;
+const extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
 NewMockResponse = (function(superClass) {
   extend(NewMockResponse, superClass);
@@ -25,16 +25,16 @@ NewMockResponse = (function(superClass) {
 })(Helper.Response);
 
 
-describe('hubot', function() {
+describe('hubot', () => {
 
-	var room;
+	let room;
 
 	beforeEach(function() { room = helper.createRoom({response: NewMockResponse})});
 	afterEach(function() { room.destroy()});
 
-	it('should respond when asked to encourage person', function(done) {
+	it('should respond when asked to encourage person', (done) => {
 
-		room.user.say('alice', 'hubot encourage bill').then(function() {
+		room.user.say('alice', 'hubot encourage bill').then(() => {
 			expect(room.messages).to.eql([
 				['alice', 'hubot encourage bill'],
 				['hubot', 'Bill is good at like, 10 times more things than I am.']
